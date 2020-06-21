bb88fcce3dd45e04324d0b621651e12522f900d8
Evgeniy Kolominskiy <kolominskiyy@gmail.com>
2020-06-21 23:40:46 +0300

    First task complete.

---
+++ index.js
@@ -0,0 +1,23 @@
+/*Task 1*/
+
+/**
+ *
+ * @type {string}
+ */
+
+const age = prompt('Enter your age, please.');
+
+function isAdult() {
+	if (age >= 18) {
+		return true
+	} else {
+		return false
+	}
+}
+
+console.log(isAdult());
+
+/*Task 2*/
+
+
+
